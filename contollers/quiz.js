220f96afb00aebca897ca740c3b37d5dd31dac5e
Rich <r.johnson8719@gmail.com>
2018-11-26 10:33:02 -0500

    module upadtes

--- contollers/quiz.js
+++ contollers/quiz.js
@@ -1,19 +1,19 @@
 const data = require('../assets/questions.json') //get questions from json array  
-// const game = data.Game.location //why doesn't this work?
 const location = 'Boston' // pull from api
 const game = data.Game.Boston
+
+// console.log(game)
 
 const QuizController = {
     getQuestion: (req,res) => {
         // need to loop through for each new question
         let questionId = req.params.id
-        let question = Game.Boston.questions[questionId + 1]
-        let answers = game.q1.options
-        
+        let question = game.questions[id + 1]
+        let options = game.question.options       
 
         res.render('index', {
             question: question,
-            answers: answers
+            options: options
         })
     }
 }
