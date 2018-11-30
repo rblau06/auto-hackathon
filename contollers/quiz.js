80fcd6b5bd4369aa86c7a3462b8f4b172f9570fc
Rich <r.johnson8719@gmail.com>
2018-11-30 12:07:13 -0500

    handlebars loop id issue

--- contollers/quiz.js
+++ contollers/quiz.js
@@ -2,18 +2,44 @@ const data = require('../assets/questions.json') //get questions from json array
 const location = 'Boston' // pull from api
 const game = data.Game.Boston.questions
 
-console.log(game[2].question)
-
 const QuizController = {
+    launch: (req, res) => {
+        const welcome = 'Welcome'
+        const start = 'question/0'
+        res.render('welcome', {
+            welcome: welcome,
+            start: start
+        })
+    },
+    
     getQuestion: (req,res) => {
         let questionId = req.params.id
-        questionId --
         let question = game[questionId].question
         let options = game[questionId].options
+        let correct = game[questionId].correct
+        let optionList = ''
+        // console.log(correct)
+        
+        options.forEach(option => {
+            optionList += `<li><a href="answer/${questionId}">${option}</a>`
+        })
+        console.log(optionList)
 
         res.render('index', {
             question: question,
-            options: options
+            options: options,
+            optionList: optionList,
+            id: questionId
+        })
+    },
+    
+    answer: (req, res) => {
+        options.forEach((option, i) => {
+            if (correct == option) {
+                //go to answers/correct
+            } else {
+                //go to answers/incorrect
+            }
         })
     }
 }
