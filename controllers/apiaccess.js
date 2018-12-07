b687e1a53bef96b8215c96dfec7c2fb3e41caf17
Bob Blau <rblau@nab.org>
2018-12-07 12:26:58 -0500

    Adding API Access and Users json file

--- controllers/apiaccess.js
+++ controllers/apiaccess.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const fs = require('fs');
 
+
 request.get('https://hackathon.abgapiservices.com/nabhackathon?where=co_rental_loc_mnemonic="BOS"',
     function rentalInfo (error, response, body) {
         // console.log('error:', error); // Print the error if one occurred
@@ -18,24 +19,32 @@ request.get('https://hackathon.abgapiservices.com/nabhackathon?where=co_rental_l
         console.log(data[rentalRecord].seat_belt_sensor);
         let seatCount = data[rentalRecord].seat_belt_sensor;
         let riders = {
-            table: []
+            Players: []
         };
+
+        console.log(Players);
+
         for (a = 1; a <= seatCount; a++ ) {
-            riders.table.push([data[a].first_name, data[a].last_name]);
+            // let fullName = riders.Players.push("First Name: " + [data[a].first_name + "Last Name: " + data[a].last_name]);
+            let firstName = [data[a].first_name];
+            let lastName = [data[a].last_name];
+            // riders.Players.push(firstName, lastName);
+            riders.Players.push("First Name: " + firstName);
+            riders.Players.push("Last Name: " + lastName);
         }
+
         let json = JSON.stringify(riders);
         // Write output to JSON File
-        fs.writeFile('avisriders.json', json, 'utf8', callback);
+        let wstream = fs.createWriteStream('./assets/avisriders.json', 'utf8');
+        // for (a = 1; a <= seatCount; a++ ) {
+        //     wstream.write(json + '\n');
+        // }
 
-        fs.readFile('avisriders.json', 'utf8', function readFileCallback(err, data){
-            if (err){
-                console.log(err);
-            } else {
-                obj = JSON.parse(data); //now it an object
-                obj.table.push([data[a].first_name, data[a].last_name]); //add some data
-                json = JSON.stringify(obj); //convert it back to json
-                fs.writeFile('avisriders.json', json, 'utf8', callback); // write it back
-            }});
-        // console.log(json);
+        // console.log(riders);
+
+        // riders.forEach(function(element) {
+        //     wstream.write(json + '\n');
+        // });
+        // wstream.end();
 
     }).auth('85df44f4d7fb477aa959a2b33aa08869', 'AfCBCEa8550246Cda59cA46a6418195f', false);
